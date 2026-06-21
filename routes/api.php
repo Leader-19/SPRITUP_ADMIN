e2579548af66576a7bac2b03be096b42eae2a29d
Leader-19 <dinleader200@gmail.com>
2026-06-21 20:58:27 +0700

    New Structure

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// list all ducument
 Route::get('/documents', [DocumentController::class, 'index']);
 Route::get('/documents/{id}/content', [DocumentController::class, 'getContent']);
 Route::get('/categories', [CategoryController::class, 'index']);
